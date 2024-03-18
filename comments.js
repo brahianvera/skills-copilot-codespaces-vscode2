09841bf6f8469792042c47a785deb3d0b23d6a55
brahianvera <brahian.verac@gmail.com>
2024-03-18 21:47:26 +0000

    Create web server

--- comments.js
+++ comments.js
@@ -1,4 +1,4 @@
-//create web server
+//Create web server
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -11,72 +11,4 @@ app.get('/', (req, res) => {
 //start the server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// Path: comments.js
-// Compare this snippet from comments.js:
-// //create web server
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-// 
-// //create a route
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-// 
-// //start the server
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
-// // Path: comments.js
-// //create web server
-const express = require('express');
-
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-//start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-//create web server
-const express = require('express');
-
-
-//create a route
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-//start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// Path: comments.js
-// Compare this snippet from comments.js:
-// //create web server
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-// 
-// //create a route
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-// 
-// //start the server
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
-// // Path: comments.js
-// //create web server
-const express = require('express');
-
-//create a route
-app.get('/', (req, res) => { 
-  res.send('Hello World!');
 });
